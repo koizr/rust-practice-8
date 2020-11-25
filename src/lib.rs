5a88d49f2b2dac57623f2477dc1978b0c69bbc6f
koizr <koizumi.ryo.dev@gmail.com>
2020-11-26 00:54:23 +0900

    test: string

--- src/lib.rs
+++ src/lib.rs
@@ -62,3 +62,31 @@ mod list {
         }
     }
 }
+
+mod string {
+    // 文字列をピッグ・ラテン(訳注: 英語の言葉遊びの一つ)に変換してください。
+    // 各単語の最初の子音は、 単語の終端に移り、"ay"が足されます。
+    // 従って、"first"は"irst-fay"になります。
+    // ただし、 母音で始まる単語には、お尻に"hay"が付け足されます("apple"は"apple-hay"になります)。
+    // UTF-8エンコードに関する詳細を心に留めておいてください！
+
+    #[allow(dead_code)]
+    fn big_latin(s: &String) -> &str {
+        ""
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use crate::string::*;
+
+        #[test]
+        fn consonant() {
+            assert_eq!(big_latin(&String::from("rust")), "ust-ray")
+        }
+
+        #[test]
+        fn vowel() {
+            assert_eq!(big_latin(&String::from("apple")), "apple-hay")
+        }
+    }
+}
