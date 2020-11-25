33d7c026547f60e25693462b0dedad7ce07ac38d
koizr <koizumi.ryo.dev@gmail.com>
2020-11-26 00:12:16 +0900

    feat: median

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,30 @@ mod list {
         sum / numbers.len() as i32
     }
 
+    #[allow(dead_code)]
+    fn median(numbers: &Vec<i32>) -> i32 {
+        let mut numbers = numbers.clone();
+        numbers.sort();
+        numbers[numbers.len() / 2]
+    }
+
     #[cfg(test)]
     mod tests {
-        use crate::list::mean;
+        use crate::list::*;
 
         #[test]
         fn get_mean() {
             assert_eq!(mean(&vec![1, 10, 2, 5]), 4);
+        }
+
+        #[test]
+        fn get_median() {
+            assert_eq!(median(&vec![5, 2, 4, 1, 3]), 3);
+        }
+
+        #[test]
+        fn get_nearly_median() {
+            assert_eq!(median(&vec![2, 4, 1, 3]), 3);
         }
     }
 }
