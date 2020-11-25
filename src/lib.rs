d64f91c4553140ca8ce173a7ae6df991832a3c72
koizr <koizumi.ryo.dev@gmail.com>
2020-11-26 00:47:14 +0900

    feat: mode

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod list {
     // - mode(最も頻繁に出現する値; ハッシュマップがここでは有効活用できるでしょう)
     // を返してください。
 
+    use std::collections::HashMap;
+
     #[allow(dead_code)]
     fn mean(numbers: &Vec<i32>) -> i32 {
         let mut sum = 0;
@@ -19,6 +21,20 @@ mod list {
         let mut numbers = numbers.clone();
         numbers.sort();
         numbers[numbers.len() / 2]
+    }
+
+    #[allow(dead_code)]
+    fn mode(numbers: &Vec<i32>) -> i32 {
+        let mut counts = HashMap::new();
+        for n in numbers {
+            let count = counts.entry(n).or_insert(0);
+            *count += 1;
+        }
+
+        let mut counts: Vec<_> = counts.iter().collect();
+        counts.sort_by(|a, b| a.1.cmp(b.1));
+        let (n, _) = counts[counts.len() - 1];
+        *n.clone()
     }
 
     #[cfg(test)]
@@ -38,6 +54,11 @@ mod list {
         #[test]
         fn get_nearly_median() {
             assert_eq!(median(&vec![2, 4, 1, 3]), 3);
+        }
+
+        #[test]
+        fn get_mode() {
+            assert_eq!(mode(&vec![2, 2, 1, 3, 2, 3]), 2);
         }
     }
 }
